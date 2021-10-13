83770e9ca49e3618dfd382f3a59ebeeabd3b982d
Nike073i <nike073i@mail.ru>
2021-10-13 20:54:58 +0400

    change project type 13.10.2021 20:54

--- FurnitureFactoryView/FormMain.Designer.cs
+++ FurnitureFactoryView/FormMain.Designer.cs
@@ -4,14 +4,14 @@ namespace FurnitureFactoryView
     partial class FormMain
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Освободить все используемые ресурсы.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,11 +21,11 @@ namespace FurnitureFactoryView
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Код, автоматически созданный конструктором форм Windows
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
         /// </summary>
         private void InitializeComponent()
         {
@@ -53,67 +53,67 @@ namespace FurnitureFactoryView
             this.createDocumentTableToolStripMenuItem,
             this.createDocumentChartToolStripMenuItem});
             this.contextMenuStrip.Name = "contextMenuStrip";
-            this.contextMenuStrip.Size = new System.Drawing.Size(306, 182);
+            this.contextMenuStrip.Size = new System.Drawing.Size(254, 142);
             // 
             // addToolStripMenuItem
             // 
             this.addToolStripMenuItem.Name = "addToolStripMenuItem";
-            this.addToolStripMenuItem.Size = new System.Drawing.Size(305, 24);
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(253, 22);
             this.addToolStripMenuItem.Text = "Добавить";
             this.addToolStripMenuItem.Click += new System.EventHandler(this.AddSupplier);
             // 
             // updateToolStripMenuItem
             // 
             this.updateToolStripMenuItem.Name = "updateToolStripMenuItem";
-            this.updateToolStripMenuItem.Size = new System.Drawing.Size(305, 24);
+            this.updateToolStripMenuItem.Size = new System.Drawing.Size(253, 22);
             this.updateToolStripMenuItem.Text = "Редактировать";
             this.updateToolStripMenuItem.Click += new System.EventHandler(this.UpdateSupplier);
             // 
             // deleteToolStripMenuItem
             // 
             this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
-            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(305, 24);
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(253, 22);
             this.deleteToolStripMenuItem.Text = "Удалить";
             this.deleteToolStripMenuItem.Click += new System.EventHandler(this.DeleteSupplier);
             // 
             // toolStripSeparator
             // 
             this.toolStripSeparator.Name = "toolStripSeparator";
-            this.toolStripSeparator.Size = new System.Drawing.Size(302, 6);
+            this.toolStripSeparator.Size = new System.Drawing.Size(250, 6);
             // 
             // createSimpleDocumentToolStripMenuItem
             // 
             this.createSimpleDocumentToolStripMenuItem.Name = "createSimpleDocumentToolStripMenuItem";
-            this.createSimpleDocumentToolStripMenuItem.Size = new System.Drawing.Size(305, 24);
+            this.createSimpleDocumentToolStripMenuItem.Size = new System.Drawing.Size(253, 22);
             this.createSimpleDocumentToolStripMenuItem.Text = "Создать простой документ";
             this.createSimpleDocumentToolStripMenuItem.Click += new System.EventHandler(this.CreateSimpleDocument);
             // 
             // createDocumentTableToolStripMenuItem
             // 
             this.createDocumentTableToolStripMenuItem.Name = "createDocumentTableToolStripMenuItem";
-            this.createDocumentTableToolStripMenuItem.Size = new System.Drawing.Size(305, 24);
+            this.createDocumentTableToolStripMenuItem.Size = new System.Drawing.Size(253, 22);
             this.createDocumentTableToolStripMenuItem.Text = "Создать документ с таблицей";
             this.createDocumentTableToolStripMenuItem.Click += new System.EventHandler(this.CreateDocumentTable);
             // 
             // createDocumentChartToolStripMenuItem
             // 
             this.createDocumentChartToolStripMenuItem.Name = "createDocumentChartToolStripMenuItem";
-            this.createDocumentChartToolStripMenuItem.Size = new System.Drawing.Size(305, 24);
+            this.createDocumentChartToolStripMenuItem.Size = new System.Drawing.Size(253, 22);
             this.createDocumentChartToolStripMenuItem.Text = "Создать документ с диаграммой";
             this.createDocumentChartToolStripMenuItem.Click += new System.EventHandler(this.CreateDocumentChart);
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 338);
             this.ContextMenuStrip = this.contextMenuStrip;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "FormMain";
             this.Text = "Поставщики";
             this.Load += new System.EventHandler(this.FormMain_Load);
             this.contextMenuStrip.ResumeLayout(false);
             this.ResumeLayout(false);
-
         }
 
         #endregion
--- FurnitureFactoryView/Program.cs
+++ FurnitureFactoryView/Program.cs
@@ -1,4 +1,4 @@
-using FurnitureFactoryBusinessLogic.BusinessLogics;
+﻿using FurnitureFactoryBusinessLogic.BusinessLogics;
 using FurnitureFactoryBusinessLogic.Interfaces;
 using FurnitureFactoryDatabaseImplement.Implements;
 using System;
@@ -11,13 +11,12 @@ namespace FurnitureFactoryView
     static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
         static void Main()
         {
             var container = BuildUnityContainer();
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(container.Resolve<FormMain>());
